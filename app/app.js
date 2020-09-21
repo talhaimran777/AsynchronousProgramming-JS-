78b5c6056ce3bfebe69064b4dc3cfad21c8b4441
Talha Imran <talhaimran284@gmail.com>
2020-09-21 23:57:34 +0500

    Implemented Callback Hell

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@
 // This will not work in node environment
 
 // Running it in browser
-const request = new XMLHttpRequest();
+
 
 // Let's add event handler to the request object to detect its different stages
 
@@ -29,39 +29,56 @@ const request = new XMLHttpRequest();
 // request.send();
 
 // Let's add the above defined code to getTodos function
-let getTodos = (cb)=>{
+let getTodos = (link , cb)=>{
+    const request = new XMLHttpRequest();
     request.addEventListener('readystatechange',()=>{
         if(request.readyState === 4 && request.status === 200){
-            cb(undefined, request.responseText);
+            return cb(undefined, request.responseText);
         }
         else if(request.readyState === 4){
-            cb('Could not fetch the data', undefined);
+            return cb('Could not fetch the data', undefined);
         }
     });
 
     //Open a request to sent to the server
-    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    // request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+
+    request.open('GET', link);
 
     // Now send the opened request
     request.send();
 }
 
 
-getTodos((err, data)=>{
-//     console.log(`
-// The Error = ${err}\nThe data  = ${data}    
-//     `);
+// getTodos((err, data)=>{
+// //     console.log(`
+// // The Error = ${err}\nThe data  = ${data}    
+// //     `);
 
-    // Convert the data from the server to js array with objects
+//     // Convert the data from the server to js array with objects
 
-    // console.log(JSON.parse(data));
-    let result = JSON.parse(data);
+//     // console.log(JSON.parse(data));
+//     let result = JSON.parse(data);
 
-    // Let's print the first object in the array
-    console.log(result[0]);
+//     // Let's print the first object in the array
+//     console.log(result);
 
-    // Access result[0].title property
-    console.log(result[0].title);
+//     // Access result[0].title property
+//     // console.log(result[0].title);
+// });
+
+
+
+getTodos('./1.json', (err, data)=>{
+    console.log(data);
+
+    getTodos('./2.json', (err, data)=>{
+        console.log(data);
+
+        getTodos('./3.json', (err, data)=>{
+            console.log(data);
+        });
+    });
 });
 
 // Running it in browser End
