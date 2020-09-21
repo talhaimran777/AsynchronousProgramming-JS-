96732724f590e7ab37f22e24ca9593fa00bc3bf6
Talha Imran <talhaimran284@gmail.com>
2020-09-21 22:16:09 +0500

    Added Error Handling

--- app/app.js
+++ app/app.js
@@ -12,8 +12,11 @@ const request = new XMLHttpRequest();
 
 // readystatechange must be in small letters
 request.addEventListener('readystatechange',()=>{
-    if(request.readyState === 4){
-        console.log(request.responseText);
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request , request.responseText);
+    }
+    else if(request.readyState === 4){
+        console.log('Could not fetch the data!');
     }
 });
 
